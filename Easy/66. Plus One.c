51ad62e54efc1528a72f958824b597b34e1bb50d
AhujaNitesh <Ahujanitesh9a.roll37@gmail.com>
2026-01-02 00:36:38 +0530

    problem 66

--- Easy/66. Plus One.c
+++ Easy/66. Plus One.c
@@ -28,3 +28,44 @@ Explanation: The array represents the integer 9.
 Incrementing by one gives 9 + 1 = 10.
 Thus, the result should be [1,0].*/
 
+/*Solution by me: -*/
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+/*
+The main logic was to divide the problem into 3 cases: -
+1. If the last digit is less than 9, we can simply increment it and return the array.
+2. If there are some 9's at the end but there is a non-9 digit before them,
+   we can set all the 9's to 0 and increment that non-9 digit by 1.
+3. If all the digits are 9's, we need to create a new array with an additional digit at the start 
+   and set it to 1(size of new array should be digitsSize + 1), and set all other digits to 0.
+*/
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    int i;
+    for(i=digitsSize-1;i>=0;i--)
+    {
+        if(digits[i]<9)
+        {
+            digits[i] += 1;
+            *returnSize = digitsSize;//(logic if i>=0 or flag == 0)
+            return digits;
+        }
+        digits[i]=0;//Used this for numbers like 899 where after the loop it returned 999
+        /*So we introduced this for terms which contained a non nine number but also had 
+        9's in them*/
+    }
+    //if(i=-1)//The loop continued till the end (no need since digit[i] == 9 now)
+    /*After this is the case handling for digit[i] == 9 as the digit was never equal to 9
+    and hence the loop continued till the very end and didn't returned digits array,
+    so the loop got over till i became equal to -1*/
+        int* newArray = (int*)malloc((digitsSize + 1) * sizeof(int));
+        newArray[0] = 1;
+        for (i = 1; i <= digitsSize; i++)
+        {
+            newArray[i] = 0;
+        }
+        *returnSize = digitsSize + 1;
+        return newArray;
+}
+
